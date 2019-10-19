3c2707b6df8bc6de5ff5951e65b1c39e8cf3acf0
mikebar12 <46402307+mikebar12@users.noreply.github.com>
2019-10-19 21:08:50 +0100

    Update lib.rs

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn classify(num: u64) -> Option<Classification> {
-    if num == 0 {
-        return None;
-    } else {
-        return Some(Classification::Faltaoalgoritmo);
-    }
-}
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
     Perfect,
     Deficient,
+}
+
+pub fn classify(num: u64) -> Option<Classification> {
+    if num == 0 {
+        return None;
+    }
+    let sum: u64 = (1..num).filter(|i| num % *i == 0).sum();
+    if sum == num {
+        Some(Classification::Perfect)
+    } else if sum < num {
+        Some(Classification::Deficient)
+    } else {
+        Some(Classification::Abundant)
+    }
 }
