d0891c348714a8cd3fc219bf4003b47391193d9c
mikebar12 <46402307+mikebar12@users.noreply.github.com>
2019-10-19 20:13:32 +0100

    Palindrome-Products
    Criação do palindromo

---
+++ palindrome-products-Commit 1/src/lib.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Palindrome {
+    pub factors: Vec<(u64, u64)>,
+}
+
+impl Palindrome {
+    /// Criação do palindromo com os seguintes factores iniciais
+    pub fn new(mut a: u64, mut b: u64) -> Palindrome {
+        if a > b {
+            std::mem::swap(&mut a, &mut b);
+        }
+        Palindrome {
+            factors: vec![(a, b)],
+        }
+    }
+
+    /// Return do valor do Palindromo
+    pub fn value(&self) -> u64 {
+        self.factors[0].0 * self.factors[0].1
+    }
+
+    /// por novos factores num palindromo ja existente
+    pub fn insert(&mut self, mut a: u64, mut b: u64) {
+        if a > b {
+            std::mem::swap(&mut a, &mut b);
+        }
+        self.factors.push((a, b));
+        self.factors.sort_unstable();
+        self.factors.dedup();
+    }
+}
+
+#[inline]
+pub fn is_palindrome(n: u64) -> bool {
+    let s = n.to_string().into_bytes();
+    s.iter().zip(s.iter().rev()).all(|(a, b)| a == b)
+}
