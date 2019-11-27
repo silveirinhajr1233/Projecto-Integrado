38595da9a0a1f54434657f44875f0cbd62197c34
mikebar12 <46402307+mikebar12@users.noreply.github.com>
2019-11-27 18:20:02 +0000

    lib.rs completo

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate chrono;
+use chrono::*;
+
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start + Duration::seconds(1e9 as i64)
+}
