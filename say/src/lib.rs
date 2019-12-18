3d72c40c0b0bdb6b4f4d9f55ccccd5ba07e7e882
zechuegui <56676136+zechuegui@users.noreply.github.com>
2019-12-18 18:30:52 +0000

    Add files via upload

--- say/src/lib.rs
+++ say/src/lib.rs
@@ -1,38 +1,70 @@
 pub fn encode(n: u64) -> String {
     match n {
-        0 => String::from("zero"),
-        1 => String::from("one"),
-        2 => String::from("two"),
-        3 => String::from("three"),
-        4 => String::from("four"),
-        5 => String::from("five"),
-        6 => String::from("six"),
-        7 => String::from("seven"),
-        8 => String::from("eight"),
-        9 => String::from("nine"),
-        10 => String::from("ten"),
-        11 => String::from("eleven"),
-        12 => String::from("twelve"),
-        13 => String::from("thirteen"),
-        14 => String::from("fourteen"),
-        15 => String::from("fifteen"),
-        16 => String::from("sixteen"),
-        17 => String::from("seventeen"),
-        18 => String::from("eighteen"),
-        19 => String::from("nineteen"),
-        20 => String::from("twenty"),
-        30 => String::from("thirty"),
-        40 => String::from("forty"),
-        50 => String::from("fifty"),
-        60 => String::from("sixty"),
-        70 => String::from("seventy"),
-        80 => String::from("eighty"),
-        90 => String::from("ninety"),
-        20...99 => format!("{}-{}", encode(10*(n/10)), encode(n%10)),
-        100...999 => format!("{} hundred {}", encode(n/100), encode(n%100)),
-        1000...999_999 => format!("{} thousand {}", encode(n/1000), encode(n%1000)),
-        1_000_000...999_999_999 => format!("{} million {}", encode(n/1_000_000), encode(n%1_000_000)),
-        1_000_000_000...999_999_999_999 => format!("{} billion {}", encode(n/1_000_000_000), encode(n%1_000_000_000)),
-        _ => panic!("Too large!"),
-    }.replace(" zero", "")
+        0 => "zero".to_string(),
+        x => num_to_text(x),
+    }
+}
+
+fn num_to_text(n: u64) -> String {
+    match n {
+        0 => "".to_string(),
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        4 => "four".to_string(),
+        5 => "five".to_string(),
+        6 => "six".to_string(),
+        7 => "seven".to_string(),
+        8 => "eight".to_string(),
+        9 => "nine".to_string(),
+        10 => "ten".to_string(),
+        11 => "eleven".to_string(),
+        12 => "twelve".to_string(),
+        13 => "thirteen".to_string(),
+        15 => "fifteen".to_string(),
+        18 => "eighteen".to_string(),
+        14..=19 => format!("{}teen", num_to_text(n - 10)),
+        20 => "twenty".to_string(),
+        30 => "thirty".to_string(),
+        40 => "forty".to_string(),
+        50 => "fifty".to_string(),
+        60 => "sixty".to_string(),
+        70 => "seventy".to_string(),
+        80 => "eighty".to_string(),
+        90 => "ninety".to_string(),
+        21..=99 => format!("{}-{}", num_to_text(n - (n % 10)), num_to_text(n % 10)),
+        100..=999 => format!("{} hundred {}", num_to_text(n / 100), num_to_text(n % 100)),
+        1000..=999_999 => format!(
+            "{} thousand {}",
+            num_to_text(n / 1000),
+            num_to_text(n % 1000)
+        ),
+        1_000_000..=999_999_999 => format!(
+            "{} million {}",
+            num_to_text(n / 1_000_000),
+            num_to_text(n % 1_000_000)
+        ),
+        1_000_000_000..=999_999_999_999 => format!(
+            "{} billion {}",
+            num_to_text(n / 1_000_000_000),
+            num_to_text(n % 1_000_000_000)
+        ),
+        1_000_000_000_000..=999_999_999_999_999 => format!(
+            "{} trillion {}",
+            num_to_text(n / 1_000_000_000_000),
+            num_to_text(n % 1_000_000_000_000)
+        ),
+        1_000_000_000_000_000..=999_999_999_999_999_999 => format!(
+            "{} quadrillion {}",
+            num_to_text(n / 1_000_000_000_000_000),
+            num_to_text(n % 1_000_000_000_000_000)
+        ),
+        _ => format!(
+            "{} quintillion {}",
+            num_to_text(n / 1_000_000_000_000_000_000),
+            num_to_text(n % 1_000_000_000_000_000_000)
+        ),
+    }
+    .trim()
+    .to_string()
 }
